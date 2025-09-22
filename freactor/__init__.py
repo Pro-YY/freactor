d1ed1557c1dc7a931027f5ffd8d5217763bb57f0
xyy <xyy@example.com>
2025-09-22 10:20:00 +0800

    bump version to 1.0.1

--- freactor/__init__.py
+++ freactor/__init__.py
@@ -2,7 +2,7 @@ from .lib import Freactor, freducer
 from .lib_async import AsyncFreactor, StatusCode, freducer as async_freducer
 
 name = "freactor"
-__version__ = "0.1.1"
+__version__ = "1.0.1"
 __all__ = [
     Freactor, freducer,
     StatusCode,
